e48baff0c0ecb0dc591911314460266473ff4d33
RabindraGhimire <rabindraghimire.rg@gmail.com>
2023-09-04 23:54:08 +0545

    End of Section 2

---
+++ client-app/src/App.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios';
+import { Header } from 'semantic-ui-react';
+
+function App(){
+  const [activities,setActivities]=useState([]);
+
+  useEffect(()=>{
+    axios.get('http://localhost:5000/api/activities')
+    .then(response=>{
+      setActivities(response.data);
+    })
+  },[])
+
+  return(
+    <div>
+    
+    <Header as='h2' icon='users' content='Activitites'/>
+    
+    
+    <ul>
+      {activities.map((activity:any)=>(
+        <li key={activity.id}>
+          {activity.title}
+        </li>
+      ))}
+    </ul>
+
+   
+    </div>
+  )
+}
+
+export default App
